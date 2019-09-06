99bc427e203d34c2c1d6d911d7b8ac67f3d64bed
Yevgeniy Prokopenko <33804747+sgttrs@users.noreply.github.com>
2019-09-07 00:38:28 +0600

    Fix not showing icon in new HA versions

--- weather-card-chart.js
+++ weather-card-chart.js
@@ -207,7 +207,7 @@ class WeatherCardChart extends Polymer.Element {
       'hail': 'hass:weather-hail',
       'lightning': 'hass:weather-lightning',
       'lightning-rainy': 'hass:weather-lightning-rainy',
-      'partlycloudy': 'hass:weather-partlycloudy',
+      'partlycloudy': 'hass:weather-partly-cloudy',
       'pouring': 'hass:weather-pouring',
       'rainy': 'hass:weather-rainy',
       'snowy': 'hass:weather-snowy',
