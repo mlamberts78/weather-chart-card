e7ac5e8062bcf2c1085589cd9d16557aa5d18321
Tero Roininen <troinine@gmail.com>
2022-02-27 17:21:31 +0200

    feat: added Finnish localization

--- src/locale.js
+++ src/locale.js
@@ -122,7 +122,7 @@ const locale = {
     'sunny': 'Soleado',
     'windy': 'Viento',
     'windy-variant': 'Viento variable'
-  }, 
+  },
   hu: {
     'tempHi': 'Max. hőmérséklet',
     'tempLo': 'Min. hőmérséklet',
@@ -215,7 +215,38 @@ const locale = {
     'sunny': 'Soligt',
     'windy': 'Blåsigt',
     'windy-variant': 'Blåsigt'
-  }
+  },
+  fi: {
+    'tempHi': 'Lämpötila ylin',
+    'tempLo': 'Lämpötila alin',
+    'precip': 'Sademäärä',
+    'units': {
+      'km/h': 'km/h',
+      'm/s': 'm/s',
+      'hPa': 'hPa',
+      'mmHg': 'mm Hg',
+      'mm': 'mm',
+      'in': 'in'
+    },
+    'cardinalDirections': [
+      'N', 'N-NE', 'NE', 'E-NE', 'E', 'E-SE', 'SE', 'S-SE',
+      'S', 'S-SW', 'SW', 'W-SW', 'W', 'W-NW', 'NW', 'N-NW', 'N'
+    ],
+    'clear-night': 'Y\u00f6, selke\u00e4\u00e4',
+    'cloudy': 'Pilvist\u00e4',
+    'fog': 'Sumuista',
+    'hail': 'Raekuuroja',
+    'lightning': 'Ukkoskuuroja',
+    'lightning-rainy': 'Ukkosta, sateista',
+    'partlycloudy': 'Osittain pilvist\u00e4',
+    'pouring': 'Kaatosadetta',
+    'rainy': 'Sateista',
+    'snowy': 'Lumisadetta',
+    'snowy-rainy': 'R\u00e4nt\u00e4sadetta',
+    'sunny': 'Aurinkoista',
+    'windy': 'Tuulista',
+    'windy-variant': 'Tuulista'
+  },
 };
 
 export default locale;
