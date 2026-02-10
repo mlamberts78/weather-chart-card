d5c67502b4b3a6594097fb065ba4fe32d98f3e53
tkotz8105 <tkotz8105@gmail.com>
2026-02-10 15:30:07 -0500

    Add alternative precip24 sensor to weather chart editor

--- src/weather-chart-card-editor.js
+++ src/weather-chart-card-editor.js
@@ -12,6 +12,7 @@ const ALT_SCHEMA = [
   { name: "dew_point", title: "Alternative dew pointsensor", selector: { entity: { domain: 'sensor' } } },
   { name: "wind_gust_speed", title: "Alternative wind gust speed sensor", selector: { entity: { domain: 'sensor' } } },
   { name: "visibility", title: "Alternative visibility sensor", selector: { entity: { domain: 'sensor' } } },
+  { name: "precip24", title: "Alternative precip24 sensor", selector: { entity: { domain: 'sensor' } } },  	
 ];
 
 class WeatherChartCardEditor extends LitElement {
